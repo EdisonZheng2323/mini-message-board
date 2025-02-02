6ddd851311fb0be5fd96e28ba17f9eee051ba2f4
EdisonZheng2323 <edisonzheng3737@gmail.com>
2025-02-02 11:57:34 -0500

    Create route to the form and form functionality

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.render("index", {title: "Mini Message Board", messages: messages});
+});
+
+app.get("/new", (req, res) => {
+  res.render("form");
+})
+
+app.post("/new", (req, res) => {
+  messages.push({text: req.body.userMessage, user: req.body.userName, added: new Date()});
+  res.redirect("/");
+})
+
+app.listen(3000);
